f7347f685cd910abc5d0cd100ff6b1e105edbc3b
erdal <cinarerdal5838@gmail.com>
2025-06-03 15:11:26 +0200

    maj diagramme de classe Bestiaire

--- zeldiablo/src/gameLaby/laby/Bestiaire.java
+++ zeldiablo/src/gameLaby/laby/Bestiaire.java
@@ -1,6 +1,6 @@
 package gameLaby.laby;
 
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 
 public class Bestiaire {
@@ -16,22 +16,15 @@ public class Bestiaire {
     }
 
     public void deplacerMontresAleatoire() {
-        for(int i = 0; i < monstres.size(); i++) {
+        for (int i = 0; i < monstres.size(); i++) {
             this.monstres.get(i).deplacerAleatoire();
         }
     }
 
     public void deplacerMontresVersHeros() {
-        for(int i = 0; i < monstres.size(); i++) {
+        for (int i = 0; i < monstres.size(); i++) {
             this.monstres.get(i).deplacerVersHeros();
         }
     }
 
-=======
-/**
- * Classe représentant les monstres.
- */
-public class Bestiaire {
-
->>>>>>> 15a8f08 (maj diagramme de classe Bestiaire)
 }
